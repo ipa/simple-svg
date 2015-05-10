40615eded53e1af3b1ae6a302211f1fd52d508bf
Iwan Paolucci <iwan.paolucci@gmail.com>
2015-05-10 22:34:08 +0200

    reformat

--- simple_svg.h
+++ simple_svg.h
@@ -51,18 +51,15 @@ namespace svg
         ss << attribute_name << "=\"" << value << unit << "\" ";
         return ss.str();
     }
-  inline
-    std::string elemStart(std::string const & element_name)
+    inline std::string elemStart(std::string const & element_name)
     {
         return "\t<" + element_name + " ";
     }
-  inline
-    std::string elemEnd(std::string const & element_name)
+    inline std::string elemEnd(std::string const & element_name)
     {
         return "</" + element_name + ">\n";
     }
-  inline
-    std::string emptyElemEnd()
+    inline std::string emptyElemEnd()
     {
         return "/>\n";
     }
@@ -106,8 +103,7 @@ namespace svg
         double x;
         double y;
     };
-  inline
-    optional<Point> getMinPoint(std::vector<Point> const & points)
+    inline optional<Point> getMinPoint(std::vector<Point> const & points)
     {
         if (points.empty())
             return optional<Point>();
@@ -121,8 +117,7 @@ namespace svg
         }
         return optional<Point>(min);
     }
-  inline
-    optional<Point> getMaxPoint(std::vector<Point> const & points)
+    inline optional<Point> getMaxPoint(std::vector<Point> const & points)
     {
         if (points.empty())
             return optional<Point>();
@@ -152,24 +147,21 @@ namespace svg
     };
 
     // Convert coordinates in user space to SVG native space.
-  inline
-    double translateX(double x, Layout const & layout)
+    inline double translateX(double x, Layout const & layout)
     {
         if (layout.origin == Layout::BottomRight || layout.origin == Layout::TopRight)
             return layout.dimensions.width - ((x + layout.origin_offset.x) * layout.scale);
         else
             return (layout.origin_offset.x + x) * layout.scale;
     }
-  inline
-    double translateY(double y, Layout const & layout)
+    inline double translateY(double y, Layout const & layout)
     {
         if (layout.origin == Layout::BottomLeft || layout.origin == Layout::BottomRight)
             return layout.dimensions.height - ((y + layout.origin_offset.y) * layout.scale);
         else
             return (layout.origin_offset.y + y) * layout.scale;
-    }
-  inline
-    double translateScale(double dimension, Layout const & layout)
+    } 
+    inline double translateScale(double dimension, Layout const & layout)
     {
         return dimension * layout.scale;
     }
